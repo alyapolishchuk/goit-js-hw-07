f7af871e0c6f998f47ae7b5566289a1d832a1853
alyapolishchuk <alinochkarul081@gmail.com>
2022-06-16 23:58:58 +0300

    commmm

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const div = document.querySelector('.gallery');
+const newGallery = createGallery(galleryItems);
+let imgOrg;
+
+div.insertAdjacentHTML('beforeend', newGallery);
+
+function createGallery(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+    </a>
+    </div>`;
+        })
+        .join("");
+}
+
+div.addEventListener("click", event => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    imgOrg = basicLightbox.create(`
+    <img src=${event.target.dataset.source} width="500" height="500">`,
+        {
+            onShow: () => {
+                window.addEventListener("keydown", closeModal);
+            },
+            onClose: () => {
+                window.removeEventListener('keydown', closeModal);
+            },
+        }
+    );
+    imgOrg.show();
+});
+
+function closeModal(event) {
+    if (event.code !== "Escape") {
+        return;
+    }
+    imgOrg.close();
+}
+
